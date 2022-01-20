a7a9e9d90d8c6b62a395111e3e849199cea0bb67
TheoKondak <thkondak@gmail.com>
2022-01-20 12:31:36 +0100

    updated fullScreenMenuGrid. New approach, now renders all images at once

--- src/js/components/fullScreenMenuGrid.js
+++ src/js/components/fullScreenMenuGrid.js
@@ -2,7 +2,6 @@ import { getSin } from '../getSin.js';
 
 const data = await getSin(); // Get sin calls the sin.json file options for JS related options.
 
-const container = document.querySelector('.menu-grid--container'); // Parent Container to which the background image will be attached
 const backgroundImageContainer = document.querySelector('.menu-grid--background-container');
 const backgroundImage = document.querySelector('.menu-grid--background');
 const menuItemsArray = []; // An array that will hold all the menu items
@@ -13,11 +12,11 @@ export const fullScreenMenuGrid = () => {
   const menuItemsContent = [];
   let menuItemClass = '';
 
+  // For each menu item
   menuItemsArray.forEach((item) => {
-    // For each menu item
-
+    // For each child node
     item.childNodes.forEach((childNode) => {
-      // For each child node
+      // Add class to items
       menuItemClass = typeof childNode.innerText != 'undefined' && childNode.innerText; // For some reason for each item i get undefined. I can't solve it now, but this is a workaround.
     });
 
@@ -26,37 +25,48 @@ export const fullScreenMenuGrid = () => {
     item.classList.add(menuItemClass.toLocaleLowerCase()); // Add a unique class to the menu grid item
   });
 
+  setTimeout(() => {
+    console.log('this is the first message');
+  }, 5000);
+  setTimeout(() => {
+    console.log('this is the second message');
+  }, 3000);
+
   menuItemsContent.forEach((menuItem) => {
     const newImage = document.createElement('img');
 
-    // ENTERED ITEM
-    document.querySelector('.' + menuItem).addEventListener('mouseenter', (e) => {
-      for (const menuItemId in data.fullScreenMenuGrid.background) {
-        const currentMenuKey = Object.keys(data.fullScreenMenuGrid.background[menuItemId]);
-        const currentMenuUrl = Object.values(data.fullScreenMenuGrid.background[menuItemId]);
+    for (const menuItemId in data.fullScreenMenuGrid.background) {
+      const currentMenuKey = Object.keys(data.fullScreenMenuGrid.background[menuItemId]);
+      const currentMenuUrl = Object.values(data.fullScreenMenuGrid.background[menuItemId]);
 
-        if (menuItem == currentMenuKey) {
-          setTimeout(function () {
-            // container.style.backgroundImage = "url('" + currentMenuUrl + "')"; // Old Aproach for background-image properties
-            // backgroundImage.src = backgroundImageContainer; // Works with Image HTML element
-            backgroundImageContainer.classList.add('scale-in');
-          }, 100);
-          newImage.src = currentMenuUrl;
-          backgroundImageContainer.insertBefore(newImage, document.querySelector('.default-image')).classList.add('menu-grid--background', 'fade-in');
-        }
+      if (menuItem == currentMenuKey) {
+        setTimeout(function () {
+          backgroundImageContainer.classList.add('scale-in');
+        }, 100);
+
+        newImage.src = currentMenuUrl;
+        backgroundImageContainer.insertBefore(newImage, document.querySelector('.default-image')).classList.add('menu-grid--background', 'hidden', menuItem);
       }
-    });
 
-    // LEFT ITEM
-    document.querySelector('.' + menuItem).addEventListener('mouseleave', (e) => {
-      backgroundImage.classList.remove('fade-in');
-      backgroundImageContainer.classList.remove('scale-in');
-      backgroundImage.classList.add('fade-out');
+      // ENTERED ITEM
+      document.querySelector('.' + menuItem).addEventListener('mouseenter', (e) => {
+        newImage.classList.remove('hidden');
+        newImage.classList.add('fade-in');
+        setTimeout(() => {
+          backgroundImageContainer.classList.add('scale-in');
+          console.log('lol');
+        }, 1000);
+      });
 
-      setTimeout(() => {
-        backgroundImageContainer.removeChild(newImage);
-      }),
-        3800;
-    });
+      // LEFT ITEM
+      document.querySelector('.' + menuItem).addEventListener('mouseleave', (e) => {
+        newImage.classList.remove('fade-in');
+
+        newImage.classList.add('hidden');
+
+        backgroundImageContainer.classList.remove('scale-in');
+        setTimeout(() => {}, 800);
+      });
+    }
   });
 };
