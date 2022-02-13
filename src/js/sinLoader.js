1959bd28eef5d98572216ef2700efe29e115d1a0
TheoKondak <thkondak@gmail.com>
2022-02-13 23:51:09 +0100

    MenuBgOnScroll needs to be updated to new approach

--- src/js/sinLoader.js
+++ src/js/sinLoader.js
@@ -1,9 +1,10 @@
 import { MenuBgOnScroll } from './components/MenuBgOnScroll.js';
 import { FullScreenMenuGrid } from './components/FullScreenMenuGrid.js';
 import { CheckOverflow } from './components/CheckOverflow.js';
-import { getSin } from './getSin.js';
+
+// MenuBgOnScroll is broken, needs to be converted.
 
 //⚔️⚰️⚰️⚰️⚰️⚰️⚰️⚰️⚔️
 // Exports
 //⚔️⚰️⚰️⚰️⚰️⚰️⚰️⚰️⚔️
-export { getSin, FullScreenMenuGrid, MenuBgOnScroll, CheckOverflow };
+export { FullScreenMenuGrid, MenuBgOnScroll, CheckOverflow };
