f0332047055de0f4cf611a4ed25fc068826ea62f
TheoKondak <thkondak@gmail.com>
2022-03-24 18:47:52 +0100

    Minor udpates in Js and tests with Webpack. It works, but not enough test yet

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,6 @@
-import { fullScreenMenuGrid } from './sinLoader.js';
+import { FullScreenMenuGrid } from './sinLoader.js';
+
+import './../scss/custom.scss';
 
 //⚔️⚰️⚰️⚰️⚰️⚰️⚰️⚰️⚔️
 // Fullscreen Menu Grid
@@ -13,27 +15,27 @@ import { fullScreenMenuGrid } from './sinLoader.js';
 //   background: [{ dummy: '' }, { home: 'https://vastphotos.com//files/uploads/photos/10684/hero-l.jpg' }, { kitchens: 'https://vastphotos.com/files/uploads/photos/10702/hero-m.jpg' }, { 'modern-kitchens': 'https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg' }, { 'transitional kitchens': 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-4.jpg?w=1500&ssl=1' }, { baths: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-10.jpg?w=1500&ssl=1' }, { closets: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-3.jpg?w=1500&ssl=1' }, { about: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-12.jpg?w=1500&ssl=1' }, { contact: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-11.jpg?w=1500&ssl=1' }, { 'request a consultation': 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-6.jpg?w=1500&ssl=1' }],
 // });
 
-const homeGrid = new fullScreenMenuGrid();
+// const homeGrid = new fullScreenMenuGrid();
 
-homeGrid.init({
+// homeGrid.init({
+//   containerSelector: '#fullScreenImageGrid',
+//   backgroundContainerSelector: '.menu-grid--background-container',
+//   defaultImage: '.default-image',
+//   scaleInDelay: '100',
+//   fadeAnimationDelay: '140',
+//   background: [{ dummy: '' }, { home: 'https://vastphotos.com/files/uploads/photos/10702/hero-m.jpg' }, { kitchens: 'https://vastphotos.com//files/uploads/photos/10684/hero-l.jpg' }, { 'modern-kitchens': 'https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg' }, { 'transitional kitchens': 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-4.jpg?w=1500&ssl=1' }, { baths: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-10.jpg?w=1500&ssl=1' }, { closets: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-3.jpg?w=1500&ssl=1' }, { about: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-12.jpg?w=1500&ssl=1' }, { contact: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-11.jpg?w=1500&ssl=1' }, { 'request a consultation': 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-6.jpg?w=1500&ssl=1' }],
+// });
+
+const anotherGrid = new FullScreenMenuGrid();
+
+anotherGrid.init({
   containerSelector: '#fullScreenImageGrid',
   backgroundContainerSelector: '.menu-grid--background-container',
   defaultImage: '.default-image',
   scaleInDelay: '100',
   fadeAnimationDelay: '140',
-  background: [{ dummy: '' }, { home: 'https://vastphotos.com/files/uploads/photos/10702/hero-m.jpg' }, { kitchens: 'https://vastphotos.com//files/uploads/photos/10684/hero-l.jpg' }, { 'modern-kitchens': 'https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg' }, { 'transitional kitchens': 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-4.jpg?w=1500&ssl=1' }, { baths: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-10.jpg?w=1500&ssl=1' }, { closets: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-3.jpg?w=1500&ssl=1' }, { about: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-12.jpg?w=1500&ssl=1' }, { contact: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-11.jpg?w=1500&ssl=1' }, { 'request a consultation': 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-6.jpg?w=1500&ssl=1' }],
+  background: [{ dummy: '' }, { homes: 'https://vastphotos.com/files/uploads/photos/10702/hero-m.jpg' }, { kitchens: 'https://vastphotos.com//files/uploads/photos/10684/hero-l.jpg' }, { 'modern-kitchens': 'https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg' }, { 'transitional kitchens': 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-4.jpg?w=1500&ssl=1' }, { baths: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-10.jpg?w=1500&ssl=1' }, { closets: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-3.jpg?w=1500&ssl=1' }, { about: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-12.jpg?w=1500&ssl=1' }, { contact: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-11.jpg?w=1500&ssl=1' }, { 'request a consultation': 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-6.jpg?w=1500&ssl=1' }],
 });
-
-// const anotherGrid = new fullScreenMenuGrid();
-
-// anotherGrid.init({
-//   containerSelector: '#fullScreenImageGrid-test',
-//   backgroundContainerSelector: '.menu-grid--background-container-test',
-//   defaultImage: '.default-image-test',
-//   scaleInDelay: '100',
-//   fadeAnimationDelay: '140',
-//   background: [{ dummy: '' }, { homes: 'https://vastphotos.com/files/uploads/photos/10702/hero-m.jpg' }, { kitchensa: 'https://vastphotos.com//files/uploads/photos/10684/hero-l.jpg' }, { 'modern-kitchens': 'https://iso.500px.com/wp-content/uploads/2014/07/big-one.jpg' }, { 'transitional kitchens': 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-4.jpg?w=1500&ssl=1' }, { baaths: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-10.jpg?w=1500&ssl=1' }, { closeats: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-3.jpg?w=1500&ssl=1' }, { aboaut: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-12.jpg?w=1500&ssl=1' }, { contaact: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-11.jpg?w=1500&ssl=1' }, { 'request a consultation': 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2021/03/landscape-photography-tips-6.jpg?w=1500&ssl=1' }],
-// });
 
 // const newTest = new test();
 
