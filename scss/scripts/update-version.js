09f7fe39d1e8d63d8b1aa818e4292bfc5a97c578
TheoKondak <thkondak@gmail.com>
2022-08-02 19:35:46 +0200

    Sinister 0.6.72-beta.1 release - Fixed some bugs preventing spacing-beta rendering css rules - spacing-beta final stages of beta, soon to be released! - Updated documentation - Updated update-version script - Updated update-documentation script

--- scss/scripts/update-version.js
+++ scss/scripts/update-version.js
@@ -8,16 +8,23 @@ const { readFile, writeFile, promises: fsPromises } = require('fs');
 // Options
 const oldVersion = process.env.npm_package_version; // Get npm package version from package.json
 const newVersion = process.argv.slice(2); // Get new package version from command arguement
+
 const files = ['.sassdocrc', 'sin-core/misc/_warning-message-start.scss', 'sin-core/misc/_warning-message-eof.scss', 'scripts/update-documentation.js']; // Files that will be updated
 
 const updateVersion = (oldVersion = oldVersion, newVersion = newVersion, files = files) => {
   files.forEach((file) => {
+    console.log('====');
+    console.log('Opening file: ' + file);
+    console.log('====');
     readFile(file, 'utf-8', function (err, contents) {
       if (err) {
         console.log(err);
         return;
       }
       const replaced = contents.replace(oldVersion, newVersion);
+      console.log('----');
+      console.log('Replacing: ' + oldVersion + ' with ' + newVersion + ' in ' + file);
+      console.log('Writing File');
 
       writeFile(file, replaced, 'utf-8', function (err) {
         console.log(err);
