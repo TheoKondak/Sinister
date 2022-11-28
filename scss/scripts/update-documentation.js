0bc027b12e7b6fbe845f25247df721a850ca9fbd
TheoKondak <thkondak@gmail.com>
2022-11-28 11:42:24 +0100

    Update to v0.7.1 - Fixed some backwards compativility issues - Updated Documentation version

--- scss/scripts/update-documentation.js
+++ scss/scripts/update-documentation.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 // Then run `npm run doc` to generate the documentation and copy it to the root directory
 
 // Assets Folder
-const srcDir = `documentation/0.7.0`;
+const srcDir = `documentation/0.7.1`;
 const destDir = `../`;
 
 fse.copySync(
@@ -26,7 +26,7 @@ fse.copySync(
 );
 
 // index.html file
-fs.copyFile('documentation/0.7.0/index.html', '../index.html', (err) => {
+fs.copyFile('documentation/0.7.1/index.html', '../index.html', (err) => {
   if (err) throw err;
   console.log('source.txt was copied to destination.txt');
 });
